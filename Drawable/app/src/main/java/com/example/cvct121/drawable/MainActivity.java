0bb79a25258e9458cb4f657235e3b1cd7a22ad32
CVCT121 <CVCT121@nic.bc.ca>
2019-03-06 11:36:39 -0800

    Initial

---
+++ Drawable/app/src/main/java/com/example/cvct121/drawable/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.cvct121.drawable;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int mScore1;
+    private int mScore2;
+
+    private TextView mScoreText1;
+    private TextView mScoreText2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
